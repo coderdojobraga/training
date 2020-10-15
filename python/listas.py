498af99c9ecf94b0a68325fd08f602082bc3561c
Nelson Estevão <nelsonmestevao@gmail.com>
2020-10-15 21:49:21 +0100

    Add two python questions

---
+++ python/listas.py
@@ -0,0 +1,24 @@
+# Questões sobre Listas
+
+1. Apresente uma definição da função `def enum_from_to(a: int, b: int) ->
+List[int]` que constrói a lista dos números inteiros compreendidos entre dois
+limites.
+
+*Por exemplo:*
+
+```python
+>>> enum_from_to(1, 5)
+[1,2,3,4,5]
+```
+
+2. Apresente uma definição da função `def reverse(a: List[Any]) -> List[Any]`
+que dada uma lista calcula uma lista com os elementos dessa lista pela ordem
+inversa.
+
+*Por exemplo:*
+
+```python
+>>> reverse([10, 20, 30])
+[30,20,10]
+```
+
