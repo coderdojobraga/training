2edea234c673c16dc99e4384882beda1dfdfb52d
filipe <filipe.felicio.1999@gmail.com>
2021-05-25 15:39:26 +0100

    3

---
+++ elixir/exercicios/protein-translation/lib/protein_translation.ex
@@ -0,0 +1,71 @@
+defmodule ProteinTranslation do
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    list = aux_of_rna(rna)
+    if !Keyword.has_key?(list, :error) do
+      {:ok, Keyword.get_values(list, :ok)}
+    else
+      {:error, "invalid RNA"}
+    end
+  end
+
+
+  def aux_of_rna(""), do: []
+  def aux_of_rna(rna) do
+    tuple = of_codon(String.slice(rna,0,3))
+    if elem(tuple,1) != "STOP" do
+      [tuple|aux_of_rna(String.slice(rna,3,String.length(rna)))]
+    else
+      []
+    end
+
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    case codon do
+      "UGU" -> {:ok, "Cysteine"}
+      "UGC" -> {:ok, "Cysteine"}
+      "UUA" -> {:ok, "Leucine"}
+      "UUG" -> {:ok, "Leucine"}
+      "AUG" -> {:ok, "Methionine"}
+      "UUU" -> {:ok, "Phenylalanine"}
+      "UUC" -> {:ok, "Phenylalanine"}
+      "UCU" -> {:ok, "Serine"}
+      "UCC" -> {:ok, "Serine"}
+      "UCA" -> {:ok, "Serine"}
+      "UCG" -> {:ok, "Serine"}
+      "UGG" -> {:ok, "Tryptophan"}
+      "UAU" -> {:ok, "Tyrosine"}
+      "UAC" -> {:ok, "Tyrosine"}
+      "UAA" -> {:ok, "STOP"}
+      "UAG" -> {:ok, "STOP"}
+      "UGA" -> {:ok, "STOP"}
+      _     -> {:error, "invalid codon"}
+    end
+  end
+end
