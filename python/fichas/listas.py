72e0f346a8e2f1dc490c14245d1ee1f9144f93d1
Nelson Estevão <nelsonmestevao@gmail.com>
2021-08-13 20:16:15 +0100

    Add listas and build scripts

--- python/fichas/listas.py
+++ python/fichas/listas.py
@@ -6,8 +6,9 @@ Questões sobre Listas
 As listas são usadas para armazenar vários elementos numa única variável.
 Fazendo parte de uma das 4 estruturas de dados built-in na linguagem Python
 utilizadas para guardar coleções de dados. As restantes estruturas são o
-Conjunto (Set), o Túplo e o Dicionário.
+Conjunto, o Túplo e o Dicionário.
 """
+
 
 def enum_from_to(a: int, b: int) -> list[int]:
     """
@@ -32,7 +33,7 @@ def enum_from_to(a: int, b: int) -> list[int]:
     return result
 
 
-def reverse(a: list[int]) -> list[int]:
+def reverse(a: list) -> list:
     """
     Calcula a lista inversa.
 
@@ -59,44 +60,71 @@ def reverse(a: list[int]) -> list[int]:
     return result
 
 
-# 3. Apresente uma definição da função `def elem(a: Any, b: List[Any]) -> bool`
-# que testa se um elemento existe numa lista.
+def is_elem(elem, values: list) -> bool:
+    """
+    Verifica se um elemento existe numa lista.
 
-# *Por exemplo:*
+    Exemplos
+    --------
 
-# ```python
-# >>> elem(20, [10, 20, 30])
-# True
-# >>> elem(2, [10, 20, 30])
-# False
-# ```
+    >>> is_elem(20, [10, 20, 30])
+    True
+    >>> is_elem(2, [10, 20, 30])
+    False
+    """
+    for value in values:
+        if value == elem:
+            return True
 
-# 4. Apresente uma definição da função `def maximo(a: List[int]) -> int`
-# que, dada uma lista de inteiros, devolve o maior elemento dessa lista.
+    return False
 
-# *Por exemplo:*
 
-# ```python
-# >>> maximo([10, 20, 30])
-# 30
-# >>> maximo([0])
-# 0
-# >>> maximo([20, 78, 32, 40])
-# 78
-# ```
+def maximum(values: list[int]) -> int:
+    """
+    Retorna o valor máximo.
 
-# 5. Apresente uma definição da função `def somas_acumuladas(a: List[int]) -> List[int]`
-# que, dada uma lista de inteiros, devolve uma nova lista que contém as somas acumuladas da lista original.
+    Exemplos
+    --------
 
-# *Por exemplo:*
+    >>> maximum([50, 20, 30])
+    50
+    >>> maximum([20, 78, 32, 40])
+    78
+    >>> maximum([20, 12, 55, 90, 18])
+    90
+    >>> maximum([0])
+    0
+    """
+    result = values[0]
 
-# ```python
-# >>> somas_acumuladas([10, 20, 30])
-# [10, 30, 60]
-# >>> somas_acumuladas([3, 3, 3])
-# [3, 6, 9]
-# >>> somas_acumuladas([1, 2, 3, 4])
-# [1, 3, 6, 10]
-# >>> somas_acumuladas([5, 2, 1, 7])
-# [5, 7, 8, 15]
-# ```
+    for value in values:
+        if value > result:
+            result = value
+
+    return result
+
+
+def sums(values: list[int]) -> list[int]:
+    """
+    Calcula as somas acumuladas.
+
+    Exemplos
+    --------
+
+    >>> sums([10, 20, 30])
+    [10, 30, 60]
+    >>> sums([3, 3, 3])
+    [3, 6, 9]
+    >>> sums([1, 2, 3, 4])
+    [1, 3, 6, 10]
+    >>> sums([5, 2, 1, 7])
+    [5, 7, 8, 15]
+    """
+    results = []
+
+    aux = 0
+    for value in values:
+        aux += value
+        results.append(aux)
+
+    return results
