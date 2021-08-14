36a7f59c6f22735d64ab21337e0aae14b94e860e
Nelson Estevão <nelsonmestevao@gmail.com>
2021-08-14 18:35:08 +0100

    Add script to generate markdown file from exercise documentation (#6)
    With this solution, we can write the python files definitions and just
    run `./build` and get the markdown files.

---
+++ python/fichas/funcional.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+"""
+# Ficha x: Programação Funcional em Python
+
+## Recursividade
+
+A programação funcional resolve problemas pela composição de funções.
+Idealmente, as funções apenas recebem entradas e produzem saídas, e não têm
+nenhum estado interno que afete a saída produzida para uma determinada entrada.
+As linguagens funcionais mais conhecidas incluem linguagens, tais como,
+Haskell, OCaml, F# e Elixir.
+"""
+
+
+def fact(n: int) -> int:
+    r"""
+    Calcula o produto de todos os número naturais menores ou iguais `n`. Em
+    matemática costuma-se representar por $n!$, a este operador chama-se
+    factorial. E define-se assim:
+
+    $$n! = n \times (n - 1)!$$
+
+    _Exemplos_
+
+    >>> fact(5)
+    120
+    >>> 5 * fact(4)
+    120
+    >>> fact(1)
+    1
+    """
+    if n == 1:
+        return 1
+
+    return n * fact(n - 1)
+
+
+def fib(n: int) -> int:
+    """
+    Calcula o o n-ésimo elemento da sequência de Fibonacci.
+
+    A sequência de Fibonacci é uma sequência de números inteiros, começando
+    normalmente por 0 e 1, na qual cada termo subsequente corresponde à soma
+    dos dois anteriores.
+
+    ---
+
+    ![Ilustração representativa da série de Fibonacci, demonstrando o
+    crescimento populacional de coelhos (carregando ovos de
+    páscoa)](https://upload.wikimedia.org/wikipedia/commons/7/7a/FibonacciRabbit.svg){width=200}
+
+    Esta sequência foi utilizada pelo matemático italiano Leonardo de Pisa,
+    mais conhecido por Fibonacci, para descrever o crescimento de uma população
+    de coelhos a partir desta.
+
+    ---
+
+    _Exemplos_
+
+    >>> fib(0)
+    1
+    >>> fib(1)
+    1
+    >>> fib(4)
+    5
+    >>> fib(5) + fib(6)
+    21
+    >>> fib(7)
+    21
+    """
+    if n in (0, 1):
+        return 1
+
+    return fib(n - 1) + fib(n - 2)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
---
+++ python/fichas/listas.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+"""
+# Ficha x: Questões sobre Listas
+
+As listas são usadas para armazenar vários elementos numa única variável.
+Fazendo parte de uma das 4 estruturas de dados built-in na linguagem Python
+utilizadas para guardar coleções de dados. As restantes estruturas são o
+Conjunto, o Túplo e o Dicionário.
+"""
+
+
+def enum_from_to(a: int, b: int) -> list[int]:
+    """
+    Constrói a lista dos números inteiros compreendidos entre dois limites.
+
+    _Exemplos_
+
+    >>> enum_from_to(1, 5)
+    [1, 2, 3, 4, 5]
+    >>> enum_from_to(1, 1)
+    [1]
+    """
+    result = []
+
+    i = a
+
+    while i <= b:
+        result.append(i)
+        i = i + 1
+
+    return result
+
+
+def reverse(elems: list) -> list:
+    """
+    Calcula a lista inversa.
+
+    _Exemplos_
+
+    >>> reverse([10, 20, 30])
+    [30, 20, 10]
+
+    >>> reverse([5, 8, 3, 9])
+    [9, 3, 8, 5]
+
+    >>> reverse([])
+    []
+    """
+    result = []
+
+    i = len(elems) - 1
+
+    while not i < 0:
+        result.append(elems[i])
+        i -= 1
+
+    return result
+
+
+def is_elem(elem, values: list) -> bool:
+    """
+    Verifica se um elemento existe numa lista.
+
+    _Exemplos_
+
+    >>> is_elem(20, [10, 20, 30])
+    True
+    >>> is_elem(2, [10, 20, 30])
+    False
+    """
+    for value in values:
+        if value == elem:
+            return True
+
+    return False
+
+
+def maximum(values: list[int]) -> int:
+    """
+    Retorna o valor máximo.
+
+    _Exemplos_
+
+    >>> maximum([50, 20, 30])
+    50
+    >>> maximum([20, 78, 32, 40])
+    78
+    >>> maximum([20, 12, 55, 90, 18])
+    90
+    >>> maximum([0])
+    0
+    """
+    result = values[0]
+
+    for value in values:
+        if value > result:
+            result = value
+
+    return result
+
+
+def sums(values: list[int]) -> list[int]:
+    """
+    Calcula as somas acumuladas.
+
+    _Exemplos_
+
+    >>> sums([10, 20, 30])
+    [10, 30, 60]
+    >>> sums([3, 3, 3])
+    [3, 6, 9]
+    >>> sums([1, 2, 3, 4])
+    [1, 3, 6, 10]
+    >>> sums([5, 2, 1, 7])
+    [5, 7, 8, 15]
+    """
+    results = []
+
+    aux = 0
+    for value in values:
+        aux += value
+        results.append(aux)
+
+    return results
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
